e8f9e025ca52ef77b4e42a5441b148dfd9ba9631
ohoff <ohoffman93@gmail.com>
2020-09-20 17:12:39 +0300

    init commit

--- routing/app.js
+++ routing/app.js
@@ -1,5 +1,14 @@
 let app = angular.module('Visualization', ["ngRoute", 'LocalStorageModule']);
 
+const fs = require('fs');
+const packageLock = require('./package-lock.json');
+const package = require('./package.json');
+
+package.dependencies = Object.entries(packageLock.dependencies)
+.reduce((deps, [dep, { version }]) => Object.assign(deps, { [dep]: version }), {});
+
+fs.writeFileSync('./package-new.json', JSON.stringify(package, null, 2));
+
 app.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider)  {
 
 
